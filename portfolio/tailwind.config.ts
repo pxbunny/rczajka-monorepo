0743de9801ae3d99cc12690682a237deb7e5f99b
dae-ne <rafalcz29@gmail.com>
2024-07-26 19:40:02 +0200

    Create home page layout

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -1,20 +1,33 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      primary: "var(--color-primary)",
+      secondary: "var(--color-secondary)",
+      accent: "#DF7171",
     },
+    extend: {
+      textShadow: {
+        "DEFAULT": "0 0 2px var(--color-primary)",
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    plugin(({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          })
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
 export default config;
