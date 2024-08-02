f4f75adb0a78e13df2139f6d6ef13e0d3d57bfa1
dae-ne <rafalcz29@gmail.com>
2024-08-02 19:49:18 +0200

    Update logo

--- portfolio/ui/logo.tsx
+++ portfolio/ui/logo.tsx
@@ -8,6 +8,6 @@ export type LogoProps = {
 export const Logo = ({ className }: LogoProps) => (
   <Link href="/" className={`blurrable ${className}`}>
     <div className="leading-none font-bold">Rafał Czajka</div>
-    <div className="text-[0.48em] font-medium text-accent mt-[-0.2em]">Software Developer</div>
+    <div className="text-[0.475em] font-medium text-accent mt-[-0.2em]">Software Developer</div>
   </Link>
 );
