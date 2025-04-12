a340c23b6a8607049679a82359f7e11c9beae33d
dae-ne <rafalcz29@gmail.com>
2025-04-12 18:01:41 +0200

    Set social links using js

--- apps/portfolio.v0/website/script.js
+++ apps/portfolio.v0/website/script.js
@@ -1,5 +1,42 @@
-setTimeout(() => {
-  const loader = document.querySelector('.loader');
-  loader.classList.add('loader--hidden');
-  setTimeout(() => loader.remove(), 3000);
-}, 500);
+const data = {
+  socials: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/dae-ne',
+      icon: 'fa-github'
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://github.com/dae-ne',
+      icon: 'fa-linkedin'
+    }
+  ]
+};
+
+function handleLoader() {
+  setTimeout(() => {
+    const loader = document.querySelector('.loader');
+    loader.classList.add('loader--hidden');
+    setTimeout(() => loader.remove(), 3000);
+  }, 500);
+}
+
+function setSocialLinks(socials) {
+  const template = document.querySelector('#socials-template');
+  const fragment = document.createDocumentFragment();
+
+  socials.forEach(({ name, url, icon }) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('.socials__link').setAttribute('href', url);
+    clone.querySelector('.fa').setAttribute('class', `fa ${icon}`);
+    fragment.appendChild(clone);
+  });
+
+  document.querySelectorAll('.socials').forEach((socials) => {
+    const clone = fragment.cloneNode(true);
+    socials.appendChild(clone);
+  });
+}
+
+handleLoader();
+setSocialLinks(data.socials);
