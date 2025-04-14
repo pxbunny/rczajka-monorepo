e257b2a8e42f5680a7a27f03c2123a539066ada0
dae-ne <rafalcz29@gmail.com>
2025-04-14 21:38:13 +0200

    Update code formatting

--- apps/portfolio.v0/website/script.js
+++ apps/portfolio.v0/website/script.js
@@ -2,72 +2,72 @@ import { shuffle } from './utils';
 import data from './data.json';
 
 function handleLoader() {
-  setTimeout(() => {
-    const loader = document.querySelector('.loader');
-    loader.classList.add('loader--hidden');
-    setTimeout(() => loader.remove(), 3000);
-  }, 500);
+    setTimeout(() => {
+        const loader = document.querySelector('.loader');
+        loader.classList.add('loader--hidden');
+        setTimeout(() => loader.remove(), 3000);
+    }, 500);
 }
 
 function setSocialLinks(socials) {
-  const template = document.querySelector('#social-list-item-template');
-  const fragment = document.createDocumentFragment();
+    const template = document.querySelector('#social-list-item-template');
+    const fragment = document.createDocumentFragment();
 
-  socials.forEach(({ url, icon }) => {
-    const clone = template.content.cloneNode(true);
-    clone.querySelector('.socials__link').setAttribute('href', url);
-    clone.querySelector('i').setAttribute('class', icon);
-    fragment.appendChild(clone);
-  });
+    socials.forEach(({ url, icon }) => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('.socials__link').setAttribute('href', url);
+        clone.querySelector('i').setAttribute('class', icon);
+        fragment.appendChild(clone);
+    });
 
-  document.querySelectorAll('.socials').forEach((socials) => {
-    const clone = fragment.cloneNode(true);
-    socials.appendChild(clone);
-  });
+    document.querySelectorAll('.socials').forEach((socials) => {
+        const clone = fragment.cloneNode(true);
+        socials.appendChild(clone);
+    });
 }
 
 function setCarousel(technologies) {
-  const template = document.querySelector('#carousel-item-template');
-  const fragment = document.createDocumentFragment();
+    const template = document.querySelector('#carousel-item-template');
+    const fragment = document.createDocumentFragment();
 
-  technologies.forEach(({ name, src }) => {
-    const clone = template.content.cloneNode(true);
-    const img = clone.querySelector('img');
-    img.setAttribute('src', src);
-    img.setAttribute('alt', name);
-    fragment.appendChild(clone);
-  });
+    technologies.forEach(({ name, src }) => {
+        const clone = template.content.cloneNode(true);
+        const img = clone.querySelector('img');
+        img.setAttribute('src', src);
+        img.setAttribute('alt', name);
+        fragment.appendChild(clone);
+    });
 
-  const carousel = document.querySelector('.carousel');
-  const carouselinner = carousel.querySelector('.carousel__inner');
-  carouselinner.appendChild(fragment);
+    const carousel = document.querySelector('.carousel');
+    const carouselinner = carousel.querySelector('.carousel__inner');
+    carouselinner.appendChild(fragment);
 
-  const screenWidth = window.screen.width;
-  const innerWidth = carouselinner.offsetWidth;
-  const numberOfInnerItems = Math.ceil(screenWidth / innerWidth);
+    const screenWidth = window.screen.width;
+    const innerWidth = carouselinner.offsetWidth;
+    const numberOfInnerItems = Math.ceil(screenWidth / innerWidth);
 
-  for (let i = 0; i < numberOfInnerItems; i++) {
-    const clone = carouselinner.cloneNode(true);
-    carousel.appendChild(clone);
-  }
+    for (let i = 0; i < numberOfInnerItems; i++) {
+        const clone = carouselinner.cloneNode(true);
+        carousel.appendChild(clone);
+    }
 }
 
 function handleScrollButton() {
-  window.addEventListener('scroll', () => {
-    const button = document.querySelector('.scroll-to-top');
-    window.scrollY > window.innerHeight / 2
-      ? button.classList.add('scroll-to-top--visible')
-      : button.classList.remove('scroll-to-top--visible');
-  });
+    window.addEventListener('scroll', () => {
+        const button = document.querySelector('.scroll-to-top');
+        window.scrollY > window.innerHeight / 2
+            ? button.classList.add('scroll-to-top--visible')
+            : button.classList.remove('scroll-to-top--visible');
+    });
 }
 
 (function init() {
-  const { socials, technologies } = data;
+    const { socials, technologies } = data;
 
-  shuffle(technologies);
+    shuffle(technologies);
 
-  setSocialLinks(socials);
-  setCarousel(technologies);
-  handleScrollButton();
-  handleLoader();
+    setSocialLinks(socials);
+    setCarousel(technologies);
+    handleScrollButton();
+    handleLoader();
 })();
--- apps/portfolio.v0/website/utils.js
+++ apps/portfolio.v0/website/utils.js
@@ -1,9 +1,14 @@
 export const shuffle = (array) => {
-  let currentIndex = array.length;
+    let currentIndex = array.length;
+    let randomIndex = currentIndex;
 
-  while (currentIndex > 0) {
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
+    while (currentIndex > 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
 };
