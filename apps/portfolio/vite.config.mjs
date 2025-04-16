2b9ecd3fa8e9990a02846264f2a2dd4c3bd3c792
dae-ne <rafalcz29@gmail.com>
2025-04-17 00:53:34 +0200

    Fix assets in portfolio build

--- apps/portfolio/vite.config.mjs
+++ apps/portfolio/vite.config.mjs
@@ -1,19 +1,28 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   root: './website',
   base: './',
+  publicDir: './public',
   build: {
     outDir: '../../../dist/portfolio',
     emptyOutDir: true,
-    minify: true
+    minify: true,
   },
   css: {
     preprocessorOptions: {
-      scss: { api: 'modern' }
-    }
+      scss: { api: 'modern' },
+    },
   },
   plugins: [
-
-  ]
+    viteStaticCopy({
+      targets: [
+        {
+          src: './assets',
+          dest: './',
+        },
+      ],
+    }),
+  ],
 });
